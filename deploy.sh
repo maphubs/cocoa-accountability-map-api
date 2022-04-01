be9df08c35d3f0176aba48df5fed8fea6070d860
Kristofor Carle <kris@maphubs.com>
2022-03-31 21:56:36 -0400

    update to auto build in Docker Hub

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,6 @@
 NODE_ENV=production
 PACKAGE_VERSION=`node -p "require('./version.json').version"`
 
-#docker build
-docker pull node:12-alpine
-docker build . --rm -t quay.io/maphubs/cam-api:v$PACKAGE_VERSION
-
 #commit version tag
 git add version.json
 git commit  -m "version $PACKAGE_VERSION"
@@ -13,5 +9,3 @@ git tag v$PACKAGE_VERSION
 git push origin
 git push origin v$PACKAGE_VERSION
 
-#push Docker image to repo
-docker push quay.io/maphubs/cam-api:v$PACKAGE_VERSION
